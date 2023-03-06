ec728568a5be15652a36cdb111e3415fba5e7168
tech-camp <tech-camp@O-075.local>
2023-03-06 15:58:20 +0900

    [finish]nuxt-vuex-counter

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <div>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+    </div>
+  </section>
+</template>
+
+<script>
+import Counter from '~/components/Counter.vue'
+
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
